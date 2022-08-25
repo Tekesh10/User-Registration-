8d57178827e947378f654d4acab65cf4a6df6f36
Tekesh10 <gtcsantekesh@gmail.com>
2022-08-25 17:36:54 +0530

    Added: Last Name validation

--- src/com/bridgelabz/user_registration/UserRegistrationMain.java
+++ src/com/bridgelabz/user_registration/UserRegistrationMain.java
@@ -1,11 +1,15 @@
 package com.bridgelabz.user_registration;
 
 import static com.bridgelabz.user_registration.UserRegistration.validateFirstName;
+import static com.bridgelabz.user_registration.UserRegistration.validateLastName;
 
 public class UserRegistrationMain {
     public static void main(String[] args) {
 
         String firstName = "Aman";
         System.out.println(validateFirstName(firstName));
+
+        String lastName = "Khan";
+        System.out.println(validateLastName(lastName));
     }
 }
